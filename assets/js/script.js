7fa66b8bee54cc43141695c7772d2e443de7a234
shellieD <shellie.downie13@gmail.com>
2021-11-20 23:37:50 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,14 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
-    runGame("addition");
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
 
+    runGame("addition");
+    
 });
 
 /**
@@ -25,6 +31,9 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
