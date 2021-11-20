7d7140eb3c5f2f83ea35df3fc444c164770d784f
shellieD <shellie.downie13@gmail.com>
2021-11-20 22:47:46 +0000

    Add code to increement score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right :D Smartypants!!!");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}.  The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -73,11 +75,22 @@ function calculateCorrectAnswer() {
     }
 }
 
+/** 
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
 
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
-function incrementWrongScore() {
+/** 
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
